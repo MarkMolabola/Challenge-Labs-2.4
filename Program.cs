5dc3ea80365e0dc6063b9d5fc073611d6b1b979c
MarkMolabola <mark.molabola@outlook.com>
2025-07-16 09:46:59 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Challenge_Labs_2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
