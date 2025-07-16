68d6ee8fc8280adb170a5128b90d57af46b16e04
MarkMolabola <mark.molabola@outlook.com>
2025-07-16 13:46:23 -0700

    update

--- Program.cs
+++ Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student = new Student();
+           
+
+            Console.WriteLine("-----------------------Student details----------------------");
+            Console.Write("Roll Number: ");
+            student.RollNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Name: ");
+            student.Name = Console.ReadLine();
+            Console.Write("Grades for Physics, Chemistry and Computer App: (ex: 90 80 91) ");
+            String input = Console.ReadLine();
+            string[] grades = input.Split(' ');
+            Console.WriteLine("-------------------------------------------------------------");
+
+            student.PhysicsGrade = Convert.ToDouble(grades[0]);
+            student.ChemistryGrade = Convert.ToDouble(grades[1]);
+            student.ComputerAppGrade = Convert.ToDouble(grades[2]);
+            Console.Clear();
+            Console.WriteLine(student.ToString());
+
+
+
+
+
+
         }
     }
 }
---
+++ Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_Labs_2._4
+{
+    internal class Student
+    {
+        //private double totalGrade;
+        public int RollNumber { get; set; }
+        public string Name { get; set; }
+        public double PhysicsGrade { get; set; }
+        public double ChemistryGrade { get; set; }
+        public double ComputerAppGrade { get; set; }
+        public double TotalGrade
+        {
+            get
+            {
+                return PhysicsGrade + ChemistryGrade + ComputerAppGrade;
+            }
+        }
+        public double Percentage
+        {
+            get
+            {
+                return (TotalGrade / 300);
+            }
+        }
+        public string Division
+        {
+            get
+            {
+                if (Percentage >= 60)
+                    return "First";
+                else if (Percentage >= 50)
+                    return "Second";
+                else if (Percentage >= 40)
+                    return "Third";
+                else
+                    return "Fail";
+            }
+        }
+        public override string ToString()
+        {
+            return $"----------------Student Details------------------\n" +
+                   $"Roll Number: {RollNumber}\n" +
+                   $"Name: {Name}\n" +
+                   $"Physics Grade: {PhysicsGrade}\n" +
+                   $"Chemistry Grade: {ChemistryGrade}\n" +
+                   $"Computer App Grade: {ComputerAppGrade}\n" +
+                   $"Total Grade: {TotalGrade}\n" +
+                   $"Percentage: {Percentage:P}\n" +
+                   $"Division: {Division}\n"+
+                   $"-------------------------------------------------";
+
+        }
+    }
+}
